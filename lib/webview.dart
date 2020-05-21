2886f3de304a2ab3dfa1a3195859ca1a512dbdb0
imSaharukh <sasaharukh@gmail.com>
2020-05-22 00:50:02 +0600

    webview loader

--- lib/webview.dart
+++ lib/webview.dart
@@ -1,14 +1,12 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'package:webview_flutter/webview_flutter.dart';
 
-class MyWebView extends StatelessWidget {
+class MyWebView extends StatefulWidget {
   final String title;
   final String selectedUrl;
-
-  final Completer<WebViewController> _controller =
-      Completer<WebViewController>();
 
   MyWebView({
     @required this.title,
@@ -16,17 +14,59 @@ class MyWebView extends StatelessWidget {
   });
 
   @override
+  _MyWebViewState createState() => _MyWebViewState();
+}
+
+class _MyWebViewState extends State<MyWebView> {
+  bool isLoading = false;
+  @override
+  void initState() {
+    isLoading = true;
+    super.initState();
+  }
+
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  @override
   Widget build(BuildContext context) {
+    _launchURL() async {
+      var url = widget.selectedUrl;
+      if (await canLaunch(url)) {
+        await launch(url);
+      } else {
+        throw 'Could not launch $url';
+      }
+    }
+
     return Scaffold(
         appBar: AppBar(
-          title: Text(title),
+          title: Text(widget.title),
+          actions: [
+            IconButton(icon: Icon(Icons.open_in_new), onPressed: _launchURL)
+          ],
         ),
-        body: WebView(
-          initialUrl: selectedUrl,
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (WebViewController webViewController) {
-            _controller.complete(webViewController);
-          },
+        body: Stack(
+          children: [
+            WebView(
+              initialUrl: widget.selectedUrl,
+              javascriptMode: JavascriptMode.unrestricted,
+              onWebViewCreated: (WebViewController webViewController) {
+                _controller.complete(webViewController);
+              },
+              onPageFinished: (_) {
+                setState(() {
+                  isLoading = false;
+                });
+              },
+              onWebResourceError: (error) {
+                print("weberror $error");
+              },
+            ),
+            isLoading
+                ? Center(child: CircularProgressIndicator())
+                : Container(),
+          ],
         ));
   }
 }
